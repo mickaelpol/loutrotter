cc0cb497e83bd06532eebdb7a14045d9d1fcf1db
bladess <bladess@hotmail.fr>
2017-10-09 16:57:21 +0200

    nav in progress

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,7 +1,6 @@
 <div class="container">
     <div class="row">
         <div class="col-xs-12">
-            <h1>mon super titre</h1>
         </div>
     </div>
-</div>
+</div>  
--- pages/templates/navbar.php
+++ pages/templates/navbar.php
@@ -1,9 +1,26 @@
-<nav>
-    <ul>
-        <li><a href="">1</a></li>
-        <li><a href="">2</a></li>
-        <li><a href="">3</a></li>
-        <li><a href="">4</a></li>
-        <li><a href="">5</a></li>
+<nav class="navbar navbar-fixed-top">
+<div class="container-fluid">
+    <div class="navbar-header">
+        <a class="navbar-brand" href="">
+            <span class="glyphicon glyphicon-th-list"></a>
+    </div>
+    <ul class="nav navbar-nav">
+        <li class="active"><a href="?p=accueil">Loutrotter</a></li>
+        <li><a href="">Contact</a></li>
     </ul>
+    <ul class="nav navbar-nav navbar-right">
+        <form class="navbar-form navbar-left" action="" method="post">
+            <div class="input-group">
+                <input name="s" type="text" class="form-control" placeholder="Search">
+                <div class="input-group-btn">
+                    <a class="btn" type="submit">
+                    <i class="glyphicon glyphicon-search"></i>
+                    </a>
+                </div>
+            </div>
+        </form>
+        <li><a href=""><i class="fa fa-user-plus" aria-hidden="true"></i>s'inscrire</a></li>
+        <li><a href=""><span class="glyphicon glyphicon-user"></span> se connecter</a></li>
+    </ul>
+</div>
 </nav>
