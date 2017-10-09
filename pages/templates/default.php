43e4e0e836d6bf467677392e3ad465fded9feebf
bladess <bladess@hotmail.fr>
2017-10-09 14:50:31 +0200

    include lib

--- pages/templates/default.php
+++ pages/templates/default.php
@@ -6,6 +6,9 @@
     <meta name="description" content="contenu de la description">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="assets/css/police.css">
     <!-- ////////////////////// BALISE TITLE A FINIR DE REMPLIR /////////////////////////// -->
     <title>Loutrotter voyages partage découverte cuisine continent pays avion vol bateau </title> 
 </head>
@@ -21,6 +24,6 @@
 
 <!--////////////////// INCLUDE DU FOOTER AFIN DE PAS LE REPETER SUR CHAQUE PAGES //////////////////-->
     <?php include('footer.php'); ?>
-    
+    <script src="node_modules/jquery/dist/jquery.js"></script>
 </body>
 </html>
