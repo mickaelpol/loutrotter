a8704206bc40055abd3c5698ccc4c5ffcdcb3322
bladess <bladess@hotmail.fr>
2017-10-10 15:24:11 +0200

    list de pays par continent

--- index.php
+++ index.php
@@ -38,6 +38,10 @@ include("connectionDb/connection.php");
     if($p=== 'addPays'){
         include('pages/addpays.php');
     }
+//////////// SI LA VARIABLE SÉTÉ VAUT ADDPAYS ALORS ON INCLUE LA PAGE ADDPAYS DANS $CONTENT//////// ///
+if($p=== 'listPays'){
+    include('pages/listPays.php');
+}
 
 $content = ob_get_clean();
 include('pages/templates/default.php');
