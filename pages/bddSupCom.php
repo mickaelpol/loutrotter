cefeed42ffe99208cd0248bad6f3fe8aea60f27b
bladess <bladess@hotmail.fr>
2017-10-18 15:49:52 +0200

    session commentaire

--- pages/bddSupCom.php
+++ pages/bddSupCom.php
@@ -1,4 +1,11 @@
 <?php 
+if (!isset($_SESSION['admin'])) {
+    header("Location: ?p=accueil");
+} else {
+    if ($_SESSION['admin'] === "0") {
+        header("Location: ?p=accueil");
+    }
+}
 $comId = $_GET['id'];
 
 $requete = sprintf('DELETE FROM com_commentaire WHERE com_oid = %d', $comId);
--- pages/supComArt.php
+++ pages/supComArt.php
@@ -1,4 +1,7 @@
 <?php 
+if (!isset($_SESSION['id'])) {
+    header("Location: ?p=accueil");
+} 
 $comId = $_GET['id'];
 
 $requete = sprintf('DELETE FROM com_commentaire WHERE com_oid = %d', $comId);
