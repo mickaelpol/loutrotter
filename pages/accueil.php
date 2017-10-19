f54498203e11784ac40dea2987b70fdb81dffa65
mickaelpol <mickael.devweb@gmail.com>
2017-10-19 17:16:34 +0200

    ajout responsive page accueil

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,13 +1,17 @@
 <header class="container">
     <div class="row">
-        <div class="col-sm-5 col-sm-offset-3">
-            <h1 class="titreAcc text-center">
+        <div class="col-sm-12 col-xs-12 col-lg-6 col-lg-offset-2">
+            <h1 class="titreAcc text-center hidden-xs hidden-md hidden-sm">
 
                 Lou Tr<img class="logoA" src="assets/img/Logo_Loutrotter.png" alt="Logo"/>tter
             </h1>
+            <h1 class="titre text-center visible-xs visible-md visible-sm">
+
+                Lou Trotter
+            </h1>
         </div>
-        <div class="col-sm-4">
-            <div class="text-center">
+        <div class="col-lg-4 col-sm-4 col-xs-12 hidden-xs hidden-sm hidden-md">
+            <div class="text-right">
                 <ul class = "list-unstyled list-inline">
 
                     <li><a href="https://www.facebook.com/LouTrotterBlog">
@@ -31,21 +35,33 @@
 
 </header>
 <div class="container">
-    <div class="col-sm-offset-2 col-xs-8">
+    <div class="col-sm-offset-2 col-sm-8 col-xs-12">
         <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Eos vel inventore deserunt aliquam. Deleniti, aut quibusdam
             praesentium consectetur, obcaecati recusandae quam magni
             cum iusto error voluptatum enim itaque, corporis minus.
         </p>
-        <!-- SnapWidget -->
-        <div class="col-xs-offset-1">
+    </div>
+</div>
+<div class="container">
+    <!-- SnapWidget visible pour tous les supports sauf les xs -->
+        <div class="col-sm-offset-1 hidden-xs col-lg-8 col-lg-offset-2">
             <hr>
             <iframe src="https://snapwidget.com/embed/456871" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="no"
             style="border:none; overflow:hidden; width:750px; height:125px"></iframe>
             <hr>
         </div>
-    </div>
+</div>
+<!-- widget visible uniquement sur potable -->
+<div class="container">
+    <!-- SnapWidget -->
+        <div class="col-xs-4 col-xs-offset-1 hidden-sm hidden-md hidden-lg">
+            <hr>
+            <iframe src="https://snapwidget.com/embed/456871" class="snapwidget-widget" allowTransparency="true" scrolling="no"
+            style="border:none; overflow:hidden; width:250px; height:300px"></iframe>
+            <hr>
+        </div>
 </div>
 
 
