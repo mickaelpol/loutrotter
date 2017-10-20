c44867877dde70387baab2d9803f5bbf718406e0
mickaelpol <mickael.devweb@gmail.com>
2017-10-20 18:07:49 +0200

    ajout responsive page contact

--- pages/contact.php
+++ pages/contact.php
@@ -3,15 +3,23 @@
 ?>
 <div class="container">
     <div class="row ">
-        <div class="col-xs-6 col-xs-offset-3">
+        <div class="col-sm-6 col-sm-offset-3 col-xs-8 col-xs-offset-2">
             <h1 class="titre page-header text-center animated zoomInLeft">Contactez moi !</h1>
         </div>
         <br>
     </div>
     <div class="row">
-    <div class="col-sm-offset-2 col-sm-6">
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScazx96TjpLY02cD9OZ_ZeLEeOwySqSdWoRnaytM_ggM64zOw/viewform?embedded=true" width="760" height="1200" frameborder="0" marginheight="0" marginwidth="0">Chargement en cours...</iframe>
+    <div class="col-sm-6 visible-sm hidden-md hidden-xs">
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScazx96TjpLY02cD9OZ_ZeLEeOwySqSdWoRnaytM_ggM64zOw/viewform?embedded=true" width="700" height="1200" frameborder="0" marginheight="0" marginwidth="0">Chargement en cours...</iframe>
+    </div>
+    <div class="col-xs-6 visible-xs">
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScazx96TjpLY02cD9OZ_ZeLEeOwySqSdWoRnaytM_ggM64zOw/viewform?embedded=true" width="300" height="1200" frameborder="0" marginheight="0" marginwidth="0">Chargement en cours...</iframe>
+    </div>
+    <div class="col-md-6 col-md-offset-1 visible-md">
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScazx96TjpLY02cD9OZ_ZeLEeOwySqSdWoRnaytM_ggM64zOw/viewform?embedded=true" width="800" height="1200" frameborder="0" marginheight="0" marginwidth="0">Chargement en cours...</iframe>
+    </div>
+    <div class="col-lg-6 visible-lg">
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScazx96TjpLY02cD9OZ_ZeLEeOwySqSdWoRnaytM_ggM64zOw/viewform?embedded=true" width="1200" height="1200" frameborder="0" marginheight="0" marginwidth="0">Chargement en cours...</iframe>
     </div>
     </div>
-
 </div>
