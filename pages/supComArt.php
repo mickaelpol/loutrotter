ca28dddf42f752786c9dc4af837f5c54bfeb4d9f
mystiichawk <polmickael3@gmail.com>
2017-10-23 11:20:25 +0200

    ajout responsive page modification du profil utilisateur

--- pages/supComArt.php
+++ pages/supComArt.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 if (!isset($_SESSION['id'])) {
     header("Location: ?p=accueil");
-} 
+}
 $comId = $_GET['id'];
 
 $requete = sprintf('DELETE FROM com_commentaire WHERE com_oid = %d', $comId);
